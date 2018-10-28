d03c0fc10b55aa5e0e79b2d4ee626ebe02a19862
technoir <sergey.chelombitko@gmail.com>
2018-10-28 17:09:05 +0300

    Bump versions

--- app/src/main/java/com/commit451/foregroundviews/sample/MainActivity.java
+++ app/src/main/java/com/commit451/foregroundviews/sample/MainActivity.java
@@ -1,6 +1,7 @@
 package com.commit451.foregroundviews.sample;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.widget.ImageView;
@@ -10,13 +11,13 @@ import com.squareup.picasso.Picasso;
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        ImageView image = (ImageView) findViewById(R.id.image);
+        ImageView image = findViewById(R.id.image);
         Picasso.with(this)
                 .load("http://www.androidcentral.com/sites/androidcentral.com/files/styles/large/public/article_images/2014/10/lollipop-statue-2.jpg?itok=RC1ovcEz")
                 .into(image);
